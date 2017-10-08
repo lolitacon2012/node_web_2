e11e5d8645023cfdeb7099b0551813c643f5566c
Liu Dake <lolitacon2012@gmail.com>
2017-10-08 23:26:38 +0800

    init commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+//initialize app vue
+var app = new Vue({
+  el: '#app',
+  data: {
+  	isNotOnTop: false,
+    page_title : 'Liu Dake'
+  }
+})
+
+//initialize variables
+app.isNotOnTop = ($(document).scrollTop() >= 200);
+
+//initialize events
+$(document).scroll(function() {
+  app.isNotOnTop = ($(document).scrollTop() >= 200);
+});
